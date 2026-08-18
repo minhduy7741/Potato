4b51dbf7ec3cb541cd58ea17c91dd64493c8bf57
Nguyễn Minh Duy <minhduy7741@gmail.com>
2026-08-18 22:16:28 +0700

    fix: resolve Circular dependency / DI injection error in ChatModule

--- be/src/chat/chat.module.ts
+++ be/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { SystemModule } from '../system/system.module';
 
 @Module({
+  imports: [SystemModule],
   controllers: [ChatController],
   providers: [ChatService],
 })
